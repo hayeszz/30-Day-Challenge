2fcb4b8dc558b4721b1855387328b98462f4674a
Michael Hayes <hayes.michael89@gmail.com>
2020-07-08 11:54:09 +0200

    updated day3 to add functionality

--- Day 3/script.js
+++ Day 3/script.js
@@ -3,7 +3,9 @@ const inputs = document.querySelectorAll(".controls input");
 
 function handleUpdate(){
     const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+    console.log(this.name);
+    console.log(this.value + suffix);
+    document.documentElement.style.setProperty(`--${this.name}`,`${this.value + suffix}`);
 }
 
 inputs.forEach(input => input.addEventListener("input",handleUpdate));
